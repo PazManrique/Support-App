eb1e981db7d0d6cd25132cb4ed8665a774970359
Juliet Linarez <julietlinarez@gmail.com>
2022-07-19 13:17:02 +0200

    ready success message

--- src/Views/successMessageView.php
+++ src/Views/successMessageView.php
@@ -1,7 +1,14 @@
+<div class="Contenedor">
+<div class="sms-success">
+<p>Tu edición</p>
+<p>se logro con</p>
+<p>éxito</p>
+</div>
+
 <div class="exito">
     <img src="src/assets/images/imagen-exito.png" alt="">
 </div>
-
+</div>
 <div class="mess-new-request">
 <button type="button" class="btn-new-request">Volver a Solicitudes</button>
 </div>
