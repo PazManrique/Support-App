1e5c129a3343b2c02e81ce7f1f327494aeea76bc
Marta-asturias <marta.gonzalez@domoticadavinci.com>
2022-07-19 02:05:46 +0200

    base de datos

---
+++ src/repositores/MySqlRepositores/MySqlConection.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Repositores\MySqlRepositores;
+
+use PDO;
+use PDOException;
+
+class MySqlConection {
+    private string $localhost = "localhost";
+    private string $db_name  = "solicitudes";
+    private string $username = "root";
+    private string $password = "";
+
+    public $mysql;
+
+public function __construct()
+{
+    try{
+    $this->mysql = $this->getConection();
+    } catch(PDOException $ex){
+    echo "esto es un error en db" . $ex->getMessage();
+    die();
+    }
+
+
+    
+
+}
+public function getConection()
+{
+    $charset = "utf 8";
+    $options = [PDO::FETCH_DEFAULT => PDO::FETCH_ASSOC];
+    $dns = "mysql:host={$this->localhost};dbname={$this->db_name};$charset";
+    $pdo = new PDO($dns,$this->username,$this->password,$options);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    return $pdo;
+}
+
+
+}
