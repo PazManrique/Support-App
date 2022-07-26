111c8f70240e1e5bea3f75b2e1443acec9a23aff
Nerea <fernandezcuestanerea@gmail.com>
2022-07-26 12:53:53 +0200

    pull develop

--- src/Views/layouts/form.php
+++ src/Views/layouts/form.php
@@ -1,8 +1,8 @@
 <div class="form">
-<form>
+<form action="" method="post" class="form-contain">
   <div class="mb-3">
     <label for="name" class="form-label">Nombre del solicitante</label>
-    <input type="text" class="form-control" id="name">
+    <input type="text" class="form-control" id="name" name="name">
   </div>
   <div class="mb-3">
     <label for="subject" class="form-label">Tema de la consulta</label>
@@ -12,12 +12,12 @@
   <label for="description" class="form-label">Descripción de la consulta</label>
   <textarea class="form-control" id="description" rows="3"></textarea>
 </form>
-</div>
 <br>
 <div class= "d-flex justify-content-around buttons">
-  <button type="submit" class="btn btn-primary">Resetear</button>
+<button type="reset" class="btn btn-primary" value="reset">Resetear</button>
   <button type="submit" class="btn btn-primary">Cancelar</button>
   <button type="submit" class="btn btn-primary">Guardar</button>
   </div>
+</div>
 
   
