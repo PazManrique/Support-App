07773b2a695b17f297aa4767c1c95b6a91240401
Nerea <fernandezcuestanerea@gmail.com>
2022-07-27 15:19:48 +0200

    changes buttons form

--- src/Views/layouts/form.php
+++ src/Views/layouts/form.php
@@ -11,15 +11,18 @@
   <div class="mb-3">
   <label for="description" class="form-label">Descripción de la consulta</label>
   <textarea class="form-control" name="description" rows="3"></textarea>
+  </form>
 
-  <div class= "d-flex justify-content-around buttons">
-  <button type="submit" class="btn btn-primary btn-save">Guardar</button>
-  </div>
-</form>
+  <br>
 <div class= "d-flex justify-content-around buttons">
-  <button type="reset" class="btn btn-primary btn-reset">Resetear</button>
-  <button type="submit" class="btn btn-primary btn-cancel">Cancelar</button>
-  </div>
+<button type="reset" class="btn btn-primary" value="reset">Resetear</button>
+<div class="btn-home">
+<a href="?action=home"><button type="button" class="btn btn-primary ">Cancelar</button></a>
 </div>
+  <button type="submit" class="btn btn-primary">Guardar</button>
+  </div>
+
+</div>
+
 
   
