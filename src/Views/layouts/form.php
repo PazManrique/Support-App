3f8c4cc916146e48b268506f82c8183ae91c8bf9
unknown <nautilus1985@gmail.com>
2022-07-28 14:18:16 +0200

    making all fields mandatory and erasing the error view

--- src/Views/layouts/form.php
+++ src/Views/layouts/form.php
@@ -2,15 +2,15 @@
 <form action="?action=store" method="post">
   <div class="mb-3">
     <label for="name" class="form-label">Nombre del solicitante</label>
-    <input type="text" name = "name" class="form-control" id="name">
+    <input type="text" name = "name" required class="form-control" id="name">
   </div>
   <div class="mb-3">
     <label for="topic" class="form-label">Tema de la consulta</label>
-    <input type="text"  name ="topic" class="form-control" >
+    <input type="text"  name ="topic" required class="form-control" >
   </div>
   <div class="mb-3">
   <label for="description" class="form-label">Descripción de la consulta</label>
-  <textarea class="form-control" name="description" rows="3"></textarea>
+  <textarea class="form-control" name="description" required rows="3"></textarea>
   </form>
 
   <br>
